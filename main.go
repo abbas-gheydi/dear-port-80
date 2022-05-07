23e1ce1515281ac35d7c12bb1ed804386fd99a9e
Abbas Gheydi <abbas.gheydi@gmail.com>
2022-05-07 12:13:14 +0430

    add ssh enable switch

--- main.go
+++ main.go
@@ -9,28 +9,28 @@ import (
 
 var (
 	listenAddr *string = flag.String("listen", "0.0.0.0:8080", "listen Address")
-
-	ssh *string = flag.String("ssh", "localhost:22", "ssh address")
-
-	http *string = flag.String("http", "localhost:80", "http server address")
+	ssh        *string = flag.String("ssh", "127.0.0.1:22", "ssh address")
+	enableSSH  *bool   = flag.Bool("enable_ssh", true, "enable ssh routing")
+	http       *string = flag.String("http", "127.0.0.1:80", "http server address")
 )
 
 func init() {
 	flag.Parse()
+	log.Printf("enableSSH: %v\n", *enableSSH)
+	log.Printf("SSH_Proxying: %v\n", *ssh)
+	log.Printf("HTTP_Proxying: %v\n", *http)
+	log.Println("proxy server Started and Listening on:", *listenAddr)
 
-	log.Printf("Listening: %v ,SSH_Proxying:%v ,HTTP_Proxying:%v\n", *listenAddr, *ssh, *http)
 }
 
 func main() {
 
 	listener, err := net.Listen("tcp", *listenAddr)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
-
 		inputConnection, err := listener.Accept()
 		if err != nil {
 			log.Println("error Accepting Connection", err)
@@ -39,7 +39,6 @@ func main() {
 
 		go func() {
 			var remoteService string
-
 			sshDetector := func() (bool, []byte) {
 				data := make([]byte, 256)
 				isSSH := false
@@ -49,18 +48,16 @@ func main() {
 				}
 
 				if data[0] == 'S' && data[1] == 'S' && data[2] == 'H' {
-
 					isSSH = true
 					data = data[:dataSize]
-
 				}
 				return isSSH, data
-
 			}
 
-			isSSH, data := sshDetector()
+			defer inputConnection.Close()
 
-			if isSSH {
+			isSSH, data := sshDetector()
+			if isSSH && *enableSSH {
 				remoteService = *ssh
 				log.Println(inputConnection.RemoteAddr(), "Connected as SSH Connection")
 			} else {
@@ -69,16 +66,17 @@ func main() {
 			}
 
 			outputConnection, err := net.Dial("tcp", remoteService)
+
 			if err != nil {
 				log.Println("error dialing remote addr", err)
 				return
-			}
 
+			}
+			defer outputConnection.Close()
 			outputConnection.Write(data)
 			go io.Copy(outputConnection, inputConnection)
 			io.Copy(inputConnection, outputConnection)
-			outputConnection.Close()
-			inputConnection.Close()
+
 		}()
 	}
 }
